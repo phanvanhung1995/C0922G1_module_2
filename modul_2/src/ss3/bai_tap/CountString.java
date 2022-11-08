d23991c05d4c23c8231314ac367fe61bc729f3b1
phanvanhung1995 <phanvanhung1224@gmail.com>
2022-11-08 21:15:19 +0700

    bai_tap

---
+++ modul_2/src/ss3/bai_tap/CountString.java
@@ -0,0 +1,21 @@
+package ss3.bai_tap;
+
+import java.util.Scanner;
+
+public class CountString {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String str = "phan van hung";
+        int count = 0;
+        System.out.println("nhập ký tự cần kiểm tra");
+        String kyTu = sc.nextLine();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (kyTu.charAt(0) == str.charAt(i)) {
+                count += 1;
+            }
+
+        }
+        System.out.println("vậy số ký tự giống là : " + count);
+    }
+}
--- modul_2/src/ss3/bai_tap/Main.java
+++ modul_2/src/ss3/bai_tap/Main.java
@@ -1,15 +1,33 @@
 package ss3.bai_tap;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in) ;
-        int size ;
+        Scanner sc = new Scanner(System.in);
+        int size;
         System.out.println("nhập số cạnh của ma trận");
-        size = Integer.parseInt(sc.nextLine()) ;
-        for (int i=0 ; i<size;i++) {
+        size = Integer.parseInt(sc.nextLine());
+        int[][] arr = new int[size][size];
 
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                System.out.println("nhập các phần tử của ma trận");
+                arr[i][j] = Integer.parseInt(sc.nextLine());
+            }
         }
+        System.out.print("ma trận của bạn là : ");
+        System.out.println(Arrays.deepToString(arr));
+
+        int sum = 0;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (i == j) {
+                    sum += arr[i][j];
+                }
+            }
+        }
+        System.out.println("tổng các số trên đường chéo chính là : " + sum);
     }
 }
